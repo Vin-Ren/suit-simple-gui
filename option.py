3e4e3d532ab762232650287a09b67996de84f9a8
vin-ren <65110648+vin-ren@users.noreply.github.com>
2022-04-22 23:55:10 +0900

    Created main.py and option.py
    main.py only contains main function currently empty.
    
    option.py contains the Option class and 3 initialized instances of it. [Gunting, Batu, Kertas]

---
+++ option.py
@@ -0,0 +1,37 @@
+
+
+class Option:
+    """Option With case-insensitivity"""
+    OPTIONS = {}
+    
+    def __init__(self, name: str, BeatsList: list=[]):
+        self._name = name
+        self.BeatsList = [str(option).lower() for option in BeatsList]
+        
+        self.OPTIONS[name] = self
+
+    def __repr__(self):
+        return "<Option Object Name={} BeatsList={}>".format(self.name, "[%s]" % ",".join(self.BeatsList))
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        self.OPTIONS[new_name] = self.OPTIONS.pop(self.name)
+        self._name = new_name
+
+    def add_beats(self, option):
+        self.BeatsList.append(str(option).lower())
+
+    def beats(self, other):
+        return str(other).lower() in self.BeatsList
+
+
+Batu = Option('Batu', ['Gunting'])
+Kertas = Option('Kertas', ['Batu'])
+Gunting = Option('Gunting', ['Kertas'])
