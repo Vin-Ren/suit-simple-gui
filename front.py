6a8f5f04cc9ef00141c065be50a6faa4d9b1c33d
IwanTz <81747549+iwansal64@users.noreply.github.com>
2022-04-23 05:38:15 +0700

    Update baru cuy 1.1 ygy
    nambahin wincount sama lostcount buat track udah berapa kali menang atau kalah sama tambahin keyboard listener buat kalo pencet r pas kebuka result nya gak perlu capek capek klik tombol ulang.. langsung aja klik 'R' untuk ulang

--- front.py
+++ front.py
@@ -1,10 +1,14 @@
 import random
 import PySimpleGUI as ps
-from option import Option
 import option
+
+from pynput import keyboard
+
+
 
 def enemy_pick_randomizer():
     return random.choice([option.Batu, option.Kertas, option.Gunting])
+
 
 STRING_CONVERTER = {
     "kertas":"🖐️",
@@ -23,17 +27,17 @@ CENTERWIDTH = REALWIDTH//2
 CENTERHEIGHT = REALHEIGHT//2
 
 titleSpace = 5
-
 resultLayout = [
     [ps.Text("", key="PilihanUser", font=20, background_color="#aa0000"), ps.Text("VS",background_color="#550000", font=18), ps.Text("", key="PilihanBot", font=20, background_color="#aa0000")],
     [ps.Text("", key="Keterangan", font=20, background_color="#aa0000")],
-    [ps.Button("Ulang")]
+    [ps.Button("Ulang"), ps.Text("Pencet 'R' untuk ulang")]
 ]
 
 mainLayout = [ # Sumpah ini berantakan co
     [ps.Text("-"*(CENTERWIDTH-len(TITLE)-titleSpace)+" "*titleSpace+TITLE+" "*titleSpace+"-"*(CENTERWIDTH-len(TITLE)-titleSpace))],
     [ps.Text("\n"*10)],
     [ps.Text(" "*(CENTERWIDTH-25)), ps.Button("🖐️", size=(5, 2), font=20, key="kertas", disabled_button_color="gray"), ps.Button("✌️", size=(5, 2), font=20, key="gunting", disabled_button_color="gray"), ps.Button("✊", size=(5, 2), font=20, key="batu", disabled_button_color="gray")],
+    [ps.Text(" "*(CENTERWIDTH-25)), ps.Text("Win : ", key="winCount"), ps.Text("Lost : ", key="lostCount")],
     [ps.Column(resultLayout, visible=False, justification="center", key="ResultContainer", background_color="#aa0000", size=(300, 150))]
 ]
 
@@ -42,13 +46,36 @@ lastLayout = [
     [ps.Button('Exit')]
 ]
 
+
 layout = mainLayout+lastLayout
 window = ps.Window(title=TITLE, layout=layout, size=(WIDTH, HEIGHT))
+showResult = False
+closeResult = False
+
+winCount = 0
+lostCount = 0
+
+def get_key_down(e):
+    global closeResult
+    key = ''
+    try:
+        key = e.char
+    except AttributeError:
+        key = e
+
+    if key == 'r' and showResult:
+        window["Ulang"]._ClickHandler("Ulang")
+        closeResult = True
+    
+
+listener = keyboard.Listener(on_press=get_key_down)
+listener.start()
 
 opsiPemain = ""
 run = True
 while run:
     event, value = window.read()
+
     if event == ps.WIN_CLOSED or event == "Exit":
         break
 
@@ -57,6 +84,9 @@ while run:
         window["kertas"].update(disabled=False)
         window["batu"].update(disabled=False)
         window["gunting"].update(disabled=False)
+
+        showResult = False
+        closeResult = False
 
     else:
         window["kertas"].update(disabled=True)
@@ -72,12 +102,22 @@ while run:
 
         enemyPick = enemy_pick_randomizer()
         description = opsiPemain.beats(enemyPick)
-        description = "Menang Yeyy" if description else "Kalah :("
+
+        if description:
+            winCount += 1
+        else:
+            lostCount += 1
+
+        description = "Menang Yey" if description else "Kalah :("
 
         window["PilihanUser"].update(STRING_CONVERTER[opsiPemain.__str__().lower()])
         window["PilihanBot"].update(STRING_CONVERTER[enemyPick.__str__().lower()])
         window["Keterangan"].update(description)
         window["Spaces"].update("\n"*6)
         window["ResultContainer"].update(visible=True)
+        window["winCount"].update("Win : "+str(winCount))
+        window["lostCount"].update("Lost : "+str(lostCount))
+
+        showResult = True
 
 window.close()
