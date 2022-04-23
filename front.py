f081d87c3fd07e50940b24ae81cbe51a9fb34ed5
IwanTz <81747549+iwansal64@users.noreply.github.com>
2022-04-23 07:28:48 +0700

    Update front.py
    Menambahkan fitur kalo pilihan bot sama pilihan user sama nanti bakal seri (update bersamaan sama Option.py)

--- front.py
+++ front.py
@@ -26,6 +26,8 @@ REALHEIGHT = HEIGHT//4
 CENTERWIDTH = REALWIDTH//2
 CENTERHEIGHT = REALHEIGHT//2
 
+ps.set_options(font=("Font\Twentieth Century.ttf", 12))
+
 titleSpace = 5
 resultLayout = [
     [ps.Text("", key="PilihanUser", font=20, background_color="#aa0000"), ps.Text("VS",background_color="#550000", font=18), ps.Text("", key="PilihanBot", font=20, background_color="#aa0000")],
@@ -35,9 +37,11 @@ resultLayout = [
 
 mainLayout = [ # Sumpah ini berantakan co
     [ps.Text("-"*(CENTERWIDTH-len(TITLE)-titleSpace)+" "*titleSpace+TITLE+" "*titleSpace+"-"*(CENTERWIDTH-len(TITLE)-titleSpace))],
+    [ps.DropDown],
     [ps.Text("\n"*10)],
     [ps.Text(" "*(CENTERWIDTH-25)), ps.Button("🖐️", size=(5, 2), font=20, key="kertas", disabled_button_color="gray"), ps.Button("✌️", size=(5, 2), font=20, key="gunting", disabled_button_color="gray"), ps.Button("✊", size=(5, 2), font=20, key="batu", disabled_button_color="gray")],
-    [ps.Text(" "*(CENTERWIDTH-25)), ps.Text("Win : ", key="winCount"), ps.Text("Lost : ", key="lostCount")],
+    [ps.Text(" "*(CENTERWIDTH-25)), ps.Text("Win : 0", key="winCount"), ps.Text("Lost : 0", key="lostCount")],
+    [ps.Text(" "*(CENTERWIDTH-25)), ps.Text("Highest Win Streak : 0", key="ws"), ps.Text("Highest Lose Streak : 0", key="ls")],
     [ps.Column(resultLayout, visible=False, justification="center", key="ResultContainer", background_color="#aa0000", size=(300, 150))]
 ]
 
@@ -47,13 +51,18 @@ lastLayout = [
 ]
 
 
-layout = mainLayout+lastLayout
-window = ps.Window(title=TITLE, layout=layout, size=(WIDTH, HEIGHT))
+Layout = mainLayout+lastLayout
+window = ps.Window(title=TITLE, layout=Layout, size=(WIDTH, HEIGHT))
 showResult = False
 closeResult = False
 
 winCount = 0
 lostCount = 0
+
+winStreak = 0
+loseStreak = 0
+currentWs = 0
+currentLs = 0
 
 def get_key_down(e):
     global closeResult
@@ -101,16 +110,22 @@ while run:
             opsiPemain = option.Gunting
 
         enemyPick = enemy_pick_randomizer()
-        menang = opsiPemain.beats(enemyPick)
-        
-        if menang == -1:
-            description = "Seri"
+        description = opsiPemain.beats(enemyPick)
+
+        if description:
+            winCount += 1
+            currentWs += 1
+            currentLs = 0
+            if currentWs > winStreak:
+                winStreak = currentWs
         else:
-            if menang:
-                winCount += 1
-            else:
-                lostCount += 1
-            description = "Menang Yey" if menang else "Kalah :("
+            lostCount += 1
+            currentLs += 1
+            currentWs = 0
+            if currentLs > loseStreak:
+                loseStreak = currentLs
+
+        description = "Menang Yey" if description is True else "Kalah :(" if description is False else "Seri!"
 
         window["PilihanUser"].update(STRING_CONVERTER[opsiPemain.__str__().lower()])
         window["PilihanBot"].update(STRING_CONVERTER[enemyPick.__str__().lower()])
@@ -119,6 +134,8 @@ while run:
         window["ResultContainer"].update(visible=True)
         window["winCount"].update("Win : "+str(winCount))
         window["lostCount"].update("Lost : "+str(lostCount))
+        window["ws"].update("Highest Win Streak : "+str(winStreak))
+        window["ls"].update("Highest Lose Streak : "+str(loseStreak))
 
         showResult = True
 
