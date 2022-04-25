4295677aa8a30148655cabac3848b23bc6ef21f4
vin-ren <65110648+vin-ren@users.noreply.github.com>
2022-04-25 19:32:36 +0900

    Rewritten and Improved Option
    Option has been improved.
    Option puts all data into a dict with variable name '_data'
    
    the ways of interacting with the given data is still valid as specified in 468c94bcdb5452c4c38a71bc70c6a2c24aee15d9
    
    exceptions of direct interaction with the data through such means, are listed in the 'DIRECT_MODIFIABLE_INSTANCE_ATTRS' variable.

--- option.py
+++ option.py
@@ -2,58 +2,57 @@
 
 class Option:
     """Option With case-insensitivity"""
+    SETABLE_INSTANCE_ATTRS = ['name']
     OPTIONS = {}
-    
+
     def __setattr__(self, name, value):
-        if name not in super().__getattribute__('ON_INSTANCE_ATTRS'):
-            return self.extras.__setitem__(name, value)
-        return super().__setattr__(name, value)
-    
+        return self.__setitem__(name, value)
+
     def __setitem__(self, name, value):
-        return self.extras.__setitem__(name, value)
-    
+        if name not in super().__getattribute__('SETABLE_INSTANCE_ATTRS'):
+            return self._data.__setitem__(name, value)
+        return super().__setattr__(name, value)
+
     def __getattribute__(self, name):
         try:
+            # FOR DEBUGGING
+            # print('__getattribute__: %s.%s' % (super().__getattribute__('_data').get('name'), name))
             return super().__getattribute__(name)
         except AttributeError:
-            return super().__getattribute__('extras').__getitem__(name)
-    
-    def __getitem__(self, name):
-        return self.extras.__getitem__(name)
-    
-    def __init__(self, name: str, BeatsList: list=[], **extra_attributes):
-        super().__setattr__('_name', name)
-        super().__setattr__('BeatsList', [str(option).lower() for option in BeatsList])
-        super().__setattr__('extras', extra_attributes)
+            return self._data.__getitem__(name)
 
-        self.__class__.OPTIONS[name] = self
+    def __getitem__(self, name):
+        return self._data.__getitem__(name)
+
+    def __init__(self, name: str, beatsList: list=[], **extra_attributes):
+        # DO NOT USE DUNDER VARIABLES. IT WOULD PREPEND "_<CLASSNAME>" TO THE FRONT OF THE GIVEN ATTRIBUTE NAME
+        super().__setattr__('_data', {'name':name, 'beatsList':beatsList})
+        self._data.update(extra_attributes)
         
-        d = dict(self.__class__.__dict__)
-        d.update(self.__dict__)
-        super().__setattr__('ON_INSTANCE_ATTRS', d)
+        self.OPTIONS[name] = self
 
     def __repr__(self):
-        return "<Option Object Name={} BeatsList={}>".format(self.name, "[%s]" % ",".join(self.BeatsList))
+        return "<Option Object Name={} BeatsList={}>".format(self.name, "[%s]" % ",".join(self.beatsList))
 
     def __str__(self):
         return self.name
 
     @property
     def name(self):
-        return self._name
+        return self._data.get('name')
 
     @name.setter
     def name(self, new_name):
         self.OPTIONS[new_name] = self.OPTIONS.pop(self.name)
-        self._name = new_name
+        self._data['name'] = new_name
 
     def add_beats(self, option):
-        self.BeatsList.append(str(option).lower())
+        self.beatsList.append(str(option).lower())
 
     def beats(self, other):
         if str(other).lower() == str(self).lower():
             return -1
-        return str(other).lower() in self.BeatsList
+        return str(other).lower() in self.beatsList
 
 
 Batu = Option('Batu', ['Gunting'], display_char="✊")
