e9b6a2f2825e014013637d28f7bf8f8413137ec4
vin-ren <65110648+vin-ren@users.noreply.github.com>
2022-04-25 18:57:24 +0900

    Fixed Max Recursion Error
    Fixed max recursion error but with additional performance overhead.
    
    TODO: Put all the attributes into a dictionary or subclass dictionary.

--- option.py
+++ option.py
@@ -5,7 +5,9 @@ class Option:
     OPTIONS = {}
     
     def __setattr__(self, name, value):
-        return self.extras.__setitem__(name, value)
+        if name not in super().__getattribute__('ON_INSTANCE_ATTRS'):
+            return self.extras.__setitem__(name, value)
+        return super().__setattr__(name, value)
     
     def __setitem__(self, name, value):
         return self.extras.__setitem__(name, value)
@@ -14,17 +16,21 @@ class Option:
         try:
             return super().__getattribute__(name)
         except AttributeError:
-            return self.extras.__getitem__(name)
+            return super().__getattribute__('extras').__getitem__(name)
     
     def __getitem__(self, name):
         return self.extras.__getitem__(name)
     
     def __init__(self, name: str, BeatsList: list=[], **extra_attributes):
-        self._name = name
-        self.BeatsList = [str(option).lower() for option in BeatsList]
-        self.extras = extra_attributes
+        super().__setattr__('_name', name)
+        super().__setattr__('BeatsList', [str(option).lower() for option in BeatsList])
+        super().__setattr__('extras', extra_attributes)
+
+        self.__class__.OPTIONS[name] = self
         
-        self.OPTIONS[name] = self
+        d = dict(self.__class__.__dict__)
+        d.update(self.__dict__)
+        super().__setattr__('ON_INSTANCE_ATTRS', d)
 
     def __repr__(self):
         return "<Option Object Name={} BeatsList={}>".format(self.name, "[%s]" % ",".join(self.BeatsList))
